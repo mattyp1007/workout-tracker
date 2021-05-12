d42b058ec98947cfd8252c2b49cac7752062ea5d
Matthew Parker <matt.parker1007@gmail.com>
2021-05-12 14:52:34 -0500

    delete exercise model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +17,25 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+app.get("/exercise", (err, res) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+
+  if(err) {
+    console.log(err);
+  }
+})
+
+app.put("/exercise", (err, { body }) => {
+  const data = body;
+
+
+})
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
